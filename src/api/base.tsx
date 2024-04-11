f084f9790f834f7110c6693d2ed4a7e754bc8917
skyler <pkphung2001@gmail.com>
2024-04-11 19:40:47 +0700

    u

--- src/api/base.tsx
+++ src/api/base.tsx
@@ -1,2 +1,2 @@
-// export const BaseUrl = "https://660a51910f324a9a28849fcc.mockapi.io"
-export const BaseUrl = "http://127.0.0.1:3000"
+export const BaseUrl = "https://660a51910f324a9a28849fcc.mockapi.io"
+// export const BaseUrl = "http://127.0.0.1:3000"
