6f735d403ba92ed7d98bf0905f4bda34c68a1467
skyler <pkphung2001@gmail.com>
2024-04-11 19:53:59 +0700

    u

--- src/app/users/header.tsx
+++ src/app/users/header.tsx
@@ -1,10 +1,8 @@
-import { DarkmodeButton } from "@/utilities/darkmode_button"
 
 export const Header = () => {
   return (
     <header className="">
       Header of Users
-      <DarkmodeButton />
       hehe
     </header>
   )
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,23 +1,15 @@
 'use client'
 
 import { UsersApi } from "@/api/users";
-import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
-
-
-
+interface UserType {
+  id: number;
+  email: string
+}
 export default function Users() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState<UserType[]>([])
 
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'email', headerName: 'Email', width: 200 },
-    // { field: 'name', headerName: 'Name', width: 200 },
-    // { field: 'phone', headerName: 'Phone', width: 200 },
-    // { field: 'createdAt', headerName: 'Created At', width: 200 },
-  ];
   const getUsers = async () => {
     try {
       const response = await UsersApi.index();
@@ -32,19 +24,7 @@ export default function Users() {
   }, [])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div style={{ height: '100%', width: '100%' }}>
-        <DataGrid
-          rows={users}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 25 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection
-        />
-      </div>
+      {users.map(user => <div key={user.id}>{user.email}</div>)}
     </main>
   );
 }
