0940da20522b4d19a35239f82dc086d687e52d1b
skyler <pkphung2001@gmail.com>
2024-04-12 18:45:30 +0700

    u

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,9 +3,7 @@ import { Content } from "./content";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Content></Content>
-      <Content></Content>
-      <Content></Content>
+      Home Page
     </main>
   );
 }
