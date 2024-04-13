1a47e848c7c14dbf911dbda21cedb8b7d2a4fbe6
skyler <pkphung2001@gmail.com>
2024-04-13 13:55:37 +0700

    u

--- src/app/teachers/[id]/page.tsx
+++ src/app/teachers/[id]/page.tsx
@@ -1,6 +1,11 @@
 'use client'
 
-const Teacher = ({params}) => {
+interface ParamsType {
+  params: {
+    id: number    
+  }
+}
+const Teacher = ({params}: ParamsType) => {
   return (
     <div>Teacher Detail Page {params.id}</div>
   )
