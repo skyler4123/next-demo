0c782a39d7dcb1e3d88caf9657158355b16a45c5
skyler <pkphung2001@gmail.com>
2024-04-13 17:22:52 +0700

    u

--- src/api/base.tsx
+++ src/api/base.tsx
@@ -1,2 +1,2 @@
-export const BaseUrl = "https://660fc4b6356b87a55c525ea7.mockapi.io"
+export const BaseUrl = "https://661a5b32125e9bb9f29be345.mockapi.io/api/v1"
 // export const BaseUrl = "http://127.0.0.1:3000"
--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -7,11 +7,23 @@ import Overlay from "@/components/overlay";
 import { Button } from "@/components/ui/button";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 
 export interface TeacherType {
   id: number;
   email: string;
+  name: string;
+  avatar: string;
+  createdAt: string;
 }
 
 const Teachers = () => {
@@ -40,9 +52,30 @@ const Teachers = () => {
       <ToastContainer />
       <Button onClick={fetchTeachers}>Refresh Teachers List</Button>
       <Overlay isShow={isShowOverlay} className={'flex flex-col w-full justify-center items-center ' + (isShowOverlay ? 'h-10' : 'h-full')}>
-        {teachers.map(teacher => (
-          <div key={teacher.id}>{teacher.email}</div>
-        ))}
+        <Table className=''>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">ID</TableHead>
+              <TableHead className="w-36">Avatar</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead className="text-right">Create at</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {teachers.map(teacher => (
+              <TableRow>
+              <TableCell className="font-medium">{teacher.id}</TableCell>
+              <TableCell>{teacher.avatar}</TableCell>
+              <TableCell>{teacher.name}</TableCell>
+              <TableCell>{teacher.email}</TableCell>
+              <TableCell className="text-right">{teacher.createdAt}</TableCell>
+            </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
       </Overlay>
     </div>
   )
