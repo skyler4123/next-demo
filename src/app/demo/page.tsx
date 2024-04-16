8bb9d326bcfdd00678ac1706ac5385fb87da0ad7
skyler <pkphung2001@gmail.com>
2024-04-17 03:38:16 +0700

    u

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,26 +1,32 @@
 'use client'
 
-import { useForm, SubmitHandler } from "react-hook-form"
+import { Input } from "@/components/ui/input"
+import { Controller, useForm } from "react-hook-form";
 
 
-interface IFormInput {
-  firstName: string
-  lastName: string
-  age: number
-}
 
+const Demo = () => {
+  const { control ,handleSubmit } = useForm()
 
-export default function App() {
-  const { register, handleSubmit } = useForm<IFormInput>()
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
-
-
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+  
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-      <input type="number" {...register("age", { min: 18, max: 99 })} />
-      <input type="submit" />
+      <Controller 
+        name="email"
+        control={control}
+        render={({field}) => {
+          console.log(field)
+          return (
+            <Input {...field} placeholder="hahah haha"/>
+          )
+        }}
+      />
+      <Input type="submit" value="Enter" />
     </form>
   )
 }
+
+export default Demo;
